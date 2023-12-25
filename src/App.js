76b0213c17782cce1bcd886d24a8e742585b0e6c
Vishal Singh <96664216+quantum0X@users.noreply.github.com>
2023-12-25 23:13:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Sidebar />
       <div className='w-full h-screen overflow-y-scroll bg-gray-200 p-4'>
         <div className='flex px-4 py-3 justify-between'>
-          <div className='text-lg'>Hello Shahrukh 👋,</div>
+          <div className='text-lg'>Hello Tronsouls 👋,</div>
           <div className='w-fit py-1 px-2 bg-white rounded-md text-gray-500 flex items-center gap-2 outline outline-1 outline-gray-400'>
             <FiSearch />
             <input placeholder='Search' className='outline-none max-sm:hidden' />
